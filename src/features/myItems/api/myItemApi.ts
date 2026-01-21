a2af400dad0c0482f49177e7c1ae33dc480b9feb
ryu-hyesu <aimee3994@naver.com>
2026-01-22 00:21:40 +0900

    feat: 나의 떡국 API 연동 및 커스텀 훅 제작 (#8)

---
+++ src/features/myItems/api/myItemApi.ts
@@ -0,0 +1,33 @@
+import type { ItemDetailResponse, PlacedItemResponse, UnplacedItemResponse } from '../types';
+
+import { instance } from '../../../services/axios';
+
+// [GET] 고명 컨텐츠 조회
+export const getItemDetail = async (itemId: string): Promise<ItemDetailResponse> => {
+  const { data } = await instance.get(`/tteokguk/me/items/${itemId}`);
+  return data;
+};
+
+// [PATCH] 고명 배치 및 수납
+export const updateItemPlacement = async (itemId: string): Promise<ItemDetailResponse> => {
+  const { data } = await instance.get(`/tteokguk/me/items/${itemId}`);
+  return data;
+};
+
+// [PATCH] 고명 읽음 처리
+export const readItem = async (itemId: string): Promise<ItemDetailResponse> => {
+  const { data } = await instance.get(`/tteokguk/me/items/${itemId}/read`);
+  return data;
+};
+
+// [GET] 인벤토리 고명 리스트 조회
+export const getPlacedItemList = async (): Promise<PlacedItemResponse[]> => {
+  const { data } = await instance.get(`/tteokguk/me/items/placed`);
+  return data;
+};
+
+// [GET] 배치된 고명 리스트 조회
+export const getUnPlacedItemList = async (): Promise<UnplacedItemResponse[]> => {
+  const { data } = await instance.get(`/tteokguk/me/items/unplaced`);
+  return data;
+};
---
+++ src/features/myItems/types/index.ts
@@ -0,0 +1,37 @@
+/**
+ * 나의 떡국에서 고명 컨텐츠 조회
+ */
+export interface ItemDetailResponse extends ItmeBase {
+  content: string;
+  contentType: string;
+  isRead: boolean;
+  mediaUrl: string;
+}
+
+// RESPONSE
+
+/**
+ * 공통 고명 정보 (ItemBase)
+ */
+export interface ItmeBase {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+/**
+ * 나의 떡국용 배치된 고명 정보 조회
+ */
+export interface PlacedItemResponse extends ItmeBase {
+  isUsed: boolean;
+  posX: number;
+  posY: number;
+  posZ: number;
+}
+
+/**
+ * 나의 떡국용 미배치된 고명 정보 조회
+ */
+export interface UnplacedItemResponse extends ItmeBase {
+  isRead: boolean;
+}
