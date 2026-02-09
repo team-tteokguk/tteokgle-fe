3ec07f1d23730c94ac93834730910134f8fbe98c
YEJI JUNG <yejiii1997@gmail.com>
2026-02-09 22:43:06 +0900

    feat: TanstacQuery 프로바이더 설정 위치 변경 및 App.tsx 삭제 (#24)

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
-import App from './App.tsx';
 import { router } from './router.tsx';
+import { queryClient } from './services/queryClient.ts';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
