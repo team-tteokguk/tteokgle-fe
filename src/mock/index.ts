fddd97b91a406cb8055934bf97c3d91fc6bc7871
ryu-hyesu <aimee3994@naver.com>
2026-02-12 17:00:20 +0900

    chore: import 정리 (#21)

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -2,10 +2,12 @@ import { guestBookHandlers } from './guestBookHandlers';
 import { memberHandlers } from './memberHandlers';
 import { myItemHandlers } from './myItemHandlers';
 import { notificationHandlers } from './notificationHandlers';
+import { storeHandlers } from './storeHandlers';
 
 export const handlers = [
   ...memberHandlers,
   ...myItemHandlers,
   ...guestBookHandlers,
   ...notificationHandlers,
+  ...storeHandlers,
 ];
