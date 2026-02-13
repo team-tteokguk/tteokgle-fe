b4c72ca4ee05f1d83a96a8a8b51cfe3a8650ea90
YEJI JUNG <yejiii1997@gmail.com>
2026-02-13 21:54:21 +0900

    fix: ProtectedRoute 로그인 여부 확인하는 변수 수정 (#26)

--- src/features/auth/components/ProtectedRoute.tsx
+++ src/features/auth/components/ProtectedRoute.tsx
@@ -3,10 +3,10 @@ import { Navigate, Outlet, useLocation } from 'react-router';
 import { useAuthStore } from '../../../store/auth/useAuthStore';
 
 const ProtectedRoute = () => {
-  const user = useAuthStore((state) => state.user);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const location = useLocation();
 
-  if (!user) {
+  if (!isAuthenticated) {
     return <Navigate replace state={{ from: location }} to="/auth/signin" />;
   }
 
