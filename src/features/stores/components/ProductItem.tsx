0ceadde213a043daab74f0a4eae90937794a24e6
YEJI JUNG <yejiii1997@gmail.com>
2026-03-03 16:39:33 +0900

    fix: 클릭하지 않는 요소 커서 빼기 (#44)

--- src/features/stores/components/ProductItem.tsx
+++ src/features/stores/components/ProductItem.tsx
@@ -9,7 +9,7 @@ interface ProductItemProps {
 
 export const ProductItem = ({ item }: ProductItemProps) => {
   return (
-    <li className="border-accent-main/30 bg-grad-store flex aspect-square w-full max-w-30 cursor-pointer flex-col items-center justify-center gap-1 rounded-2xl border-2">
+    <li className="border-accent-main/30 bg-grad-store flex aspect-square w-full max-w-30 flex-col items-center justify-center gap-1 rounded-2xl border-2">
       <p className="text-4xl leading-10 tracking-[0.369px]">{getItemEmoji(item.name)}</p>
       <span className="text-brand-dark text-xs leading-3.75 font-bold">
         {getItemNameKR(item.name)}
